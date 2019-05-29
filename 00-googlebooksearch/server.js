d893022209d3721e5ee4df70a5103ad1ea88158c
Ariana Melis <arianamelis@gmail.com>
2019-05-29 19:57:53 -0400

    heroku

--- 00-googlebooksearch/server.js
+++ 00-googlebooksearch/server.js
@@ -16,30 +16,14 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
-  next();
-});
-
-
-// Connect to the Mongo DB - try Heroku first
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GoogleSearch";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 // Connect to the Mongo DB
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/newsDB",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
 // Start the API server
 app.listen(PORT, () =>

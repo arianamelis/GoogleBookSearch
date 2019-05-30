3de15f2011d65fab6a9b2f455fd7ac15920f5761
Ariana Melis <arianamelis@gmail.com>
2019-05-29 20:52:03 -0400

    heroku changes

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+
+function Footer() {
+    return (    <footer className='footer'>
+    <div className='bottom'>
+    <hr />
+        <p className="pull-right">
+          <i className="fab fa-github" /> Proudly built using React.js
+        </p>
+        <a href='https://github.com/arianamelis' target='_blank' rel='noopener noreferrer'>Ariana Melis || </a>
+      &copy;
+      <a href='https://github.com/arianamelis' target='_blank' rel='noopener noreferrer'>Portfolio</a>
+    </div>
+      </footer>
+      );
+    }
+    export default Footer;
